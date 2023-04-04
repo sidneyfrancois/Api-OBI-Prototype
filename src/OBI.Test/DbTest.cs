b1ba8efc5a06c7d62f5abc548f25563a4b42379d
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-04 20:27:00 -0300

    test: add dbcontext properties to use in memory database

---
+++ src/OBI.Test/DbTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OBI.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBI.Test
+{
+    public class DbTest
+    {
+        private static DbContextOptions<UserRepository> dbContextOptions = new DbContextOptionsBuilder<UserRepository>()
+            .UseInMemoryDatabase(databaseName: "UserDbTest")
+            .Options;
+
+        UserRepository context;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+
+        }
+    }
+}
