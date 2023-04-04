60892bca40806be6ae8fbe7668c9c8a373473b28
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-04 19:35:09 -0300

    feat: add User class

---
+++ src/OBI.Business/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBI.Business.Models
+{
+    public class User : Entity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
