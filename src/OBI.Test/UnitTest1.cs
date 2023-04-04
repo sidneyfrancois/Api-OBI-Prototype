9f660c0d8dc25b6ec9351ef6b5991a7eee488f0f
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-04 20:07:39 -0300

    feat: add Unit Test project

---
+++ src/OBI.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace OBI.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
