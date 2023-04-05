29e43f5b4be2ecf540fb4aea213a36e1fa9d5c62
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-04 21:34:48 -0300

    feat: add User controller

---
+++ src/OBI.Api/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OBI.Business.Models;
+using OBI.Data.Context;
+
+namespace OBI.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/user")]
+    public class UserController : ControllerBase
+    {        
+        [HttpGet]
+        public IActionResult GetAllUsers([FromServices]MyDbContext context)
+        {
+            var users = context.Users.ToList();
+            return Ok(users);
+        }
+    }
+}
