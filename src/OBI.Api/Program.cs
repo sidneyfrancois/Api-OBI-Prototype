711c919f82ba4826de7ab6fd062d00d8e614a33c
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-17 17:50:31 -0300

    add connection string option

--- src/OBI.Api/Program.cs
+++ src/OBI.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OBI.Data.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,7 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<MyDbContext>();
+builder.Services.AddDbContext<MyDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
